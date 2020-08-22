27501605a7fc4c91d6ba303b4f3598dfd9a84799
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2020-08-22 14:56:18 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const bodyparser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
 
 const app = express();
 
@@ -13,24 +9,10 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
-app.post('/uploadimages', (req, res) => {
-    if(!req.file) {
-        return res.send({
-            success: false
-        });
-    }
-    else {
-        return res.send({
-            success: true
-        })
-    }
-});
-
-app.post('/uploadcode', (req, res) => {
-});
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
+const hostname = 'localhost';
+const port = process.env.PORT || 8080;
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 })
+
+module.exports = app;
