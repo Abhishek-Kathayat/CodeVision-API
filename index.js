49c7b918e7f9ad58bbe5079da34bff38565e9ab4
Mudit Mittal <Mudit.Mittal@unthinkable.co>
2020-08-22 15:53:58 +0530

    dummy route

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const bodyparser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
-
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+            );
+            // console.log("Next doing");
+            next();
+        });
+app.use(express.static(__dirname + '/public'));
 
-const hostname = 'localhost';
-const port = process.env.PORT || 8080;
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
-})
+const apiIde = require("./routes/ide");
+app.use('/api/ide', apiIde);
 
-module.exports = app;
+
+
+app.listen(port)
+    .once("listening", ()=>{
+        console.log("started on http://localhost:3000 ");
+    })
+    .on("error", (err)=>{
+        console.log("ERror "+ err);        
+    });
